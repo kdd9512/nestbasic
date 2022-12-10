435d714ddd8b14e8d619f743124d35591d9c0746
kdd9512 <hyunwoo8805@gmail.com>
2022-12-10 23:46:48 +0900

    20221210 2nd commit. Controller / Service 의 역할 숙지. 본 작업 전 Sample Controller / Service 삭제.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
+// Controller 만으로 다 되는거 아닌가 하는 의문을 가질 수 있는데, 
+// 아키텍처 구조상 둘을 구분하여 프로그램을 작성하는 것이 업계표준.
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [], // Controller 는 url 을 가져오고 function 을 실행한다.
+  providers: [], // Service 에서는 Controller 에서 사용 할 function 을 구현 & SQL 과 연결한다.
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// 시작은 터미널 - npm run start:dev
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
