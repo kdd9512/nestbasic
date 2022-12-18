eadf5ff70fbc46f90c26ccc9795c8f1d32b36f0b
kdd9512 <hyunwoo8805@gmail.com>
2022-12-18 22:51:38 +0900

    20221218 9th commit NestJS test 모드 실습 jest 사용법 학습. 불필요한 파일 삭제.

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -15,4 +16,42 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // 테스트할 메서드 명, 테스트 내용
+  describe("getAll", () => {
+
+    it('should return an array', () => {
+
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array); // return 값의 유형이 Array 인지 테스트.
+
+    });
+
+  })
+
+  describe("getAll", () => {
+    // it 은 테스트의 조건을 설정한다.
+    it('should return a movie', ()=> {
+      service.create({
+        title: "Test",
+        genres:['test'],
+        year:9999
+      });
+
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);  
+    });
+
+    it('should throw 404 err', () => {
+      try{
+        service.getOne(99999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);      
+        expect(e.message).toEqual('Cannot Found Movie with ID : 99999');
+      }
+    });
+
+  });
+
 });
